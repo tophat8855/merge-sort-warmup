5a457b9be0f66fb9c5e51b95dc3592205d1611cf
tophat8855 <hrfarley@gmail.com>
2015-01-23 09:32:51 -0800

    renaming stuff

--- public/javascripts/arrayMerging.js
+++ public/javascripts/arrayMerging.js
@@ -2,10 +2,10 @@ function merge(arr1, arr2) {
   var concatArray = arr1.concat(arr2);
   for (var i = 0; i < concatArray.length; i++) {
     for (var j = 0; j < concatArray.length- i - 1; j++) {
-      if(concatArray[j] > concatArray[j+1]) {
+      if(concatArray[j] > concatArray[j + 1]) {
         var holding = concatArray[j];
-        concatArray[j] = concatArray[j+1];
-        concatArray[j+1] = holding;
+        concatArray[j] = concatArray[j + 1];
+        concatArray[j + 1] = holding;
       }
     }
   }
